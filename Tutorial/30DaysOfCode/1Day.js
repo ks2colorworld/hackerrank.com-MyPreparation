522ae5f4b5fa644143b3cd27de7b4ec8e67110db
kslee <ks@colorworld.kr>
2024-05-22 22:54:09 +0900

    added hint : 1Day.js

--- Tutorial/30DaysOfCode/1Day.js
+++ Tutorial/30DaysOfCode/1Day.js
@@ -1,3 +1,7 @@
+/* 
+online : https://www.hackerrank.com/challenges/30-hello-world/problem?isFullScreen=true
+solved : https://github.com/ks2colorworld/hackerrank.com-MyPreparation/commit/ccb9459ea3933efdf6f286008de8c57047a58b6a
+ */
 function processData(inputString) {
     // This line of code prints the first line of output
     console.log("Hello, World.");
