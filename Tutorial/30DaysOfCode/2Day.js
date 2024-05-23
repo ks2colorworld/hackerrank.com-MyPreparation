3f600e8b417b4e96e33dea6b962d415e9719cca9
kslee <ks@colorworld.kr>
2024-05-23 12:37:12 +0900

    modified:   2Day.js

--- Tutorial/30DaysOfCode/2Day.js
+++ Tutorial/30DaysOfCode/2Day.js
@@ -24,14 +24,16 @@ function main() {
   var d = 4.0
   var s = "HackerRank "
   // Declare second integer, double, and String variables.
-
+  let myInt, myDouble, myString
   // Read and save an integer, double, and String to your variables.
-
+  myInt = parseInt(readLine())
+  myDouble = parseFloat(readLine())
+  myString = readLine()
   // Print the sum of both integer variables on a new line.
-
+  console.log(i + myInt)
   // Print the sum of the double variables on a new line.
-
+  console.log((d + myDouble).toFixed(1))
   // Concatenate and print the String variables on a new line
   // The 's' variable above should be printed first.
-
+  console.log(s + myString)
 }
