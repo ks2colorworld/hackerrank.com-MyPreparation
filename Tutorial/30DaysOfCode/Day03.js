a0e105db751de7dcc38eb6eef7a2f4f36e6cf642
kslee <ks@colorworld.kr>
2024-05-26 12:39:21 +0900

    modified:   Day03.js

--- Tutorial/30DaysOfCode/Day03.js
+++ Tutorial/30DaysOfCode/Day03.js
@@ -25,5 +25,54 @@ function readLine() {
 function main() {
   const N = parseInt(readLine().trim(), 10);
   // online : https://www.hackerrank.com/challenges/30-conditional-statements/problem?isFullScreen=true
-  
+  // console.log(N);
+  const isOdd = (N % 2) !== 0;
+  const isEven = !isOdd;
+  if (isOdd) {
+    console.log('Weird');
+  }
+  if (isEven && (1 < N && N < 6)) {
+    console.log('Not Weird');
+  }
+  if (isEven && (5 < N && N < 21)) {
+    console.log('Weird');
+  }
+  if (isEven && (20 < N)) {
+    console.log('Not Weird');
+  }
+  /* 
+  while (true) {
+    let n = parseInt(readLine().trim(), 10);
+    if (!n) {
+      break;
+    }
+    console.log(n);
+    func(n);
+  }
+  // */
 }
+
+function func(N) {
+  const isOdd = (N % 2) !== 0;
+  const isEven = !isOdd;
+  if (isOdd) {
+    console.log('Weird');
+    return;
+  }
+  if (isEven && (1 < N && N < 6) ) {
+    console.log('Not Weird');
+    return;
+  }
+  if (isEven && (5 < N && N < 21)) {
+    console.log('Weird');
+    return;
+  }
+  if (isEven && (20 < N)) {
+    console.log('Not Weird');
+    return;
+  }
+}
+
+/* 
+node Day03.js < Day03-input.txt 
+ */
