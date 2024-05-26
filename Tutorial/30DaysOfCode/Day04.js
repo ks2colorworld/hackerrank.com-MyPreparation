729b1591ef18c7b96a8a6293f12d3ee933d91ef7
kslee <ks@colorworld.kr>
2024-05-26 17:49:49 +0900

    modified:   Day04.js

--- Tutorial/30DaysOfCode/Day04.js
+++ Tutorial/30DaysOfCode/Day04.js
@@ -21,14 +21,46 @@ function readLine() {
 function Person(initialAge) {
   // Add some more code to run some checks on initialAge
   // online: https://www.hackerrank.com/challenges/30-class-vs-instance/problem?isFullScreen=true
+  let age = parseInt(initialAge);
+  console.log(age);
+  // const isEndLine = input_currentline == input_stdin_array.length;
+  // let isFirstCheckOld = true;
+  // let countYearPass = 0;
+  if (age<0) {
+    console.log('Age is not valid, setting age to 0.');
+  }
   this.amIOld = function () {
     // Do some computations in here and print out the correct statement to the console
-
+    // let msg = '';
+    if (age < 13) {
+      console.log('You are young.');
+      // msg = 'You are young.';
+    } else if (12 < age && age < 18) {
+      console.log('You are a teenager.');
+      // msg = 'You are a teenager.';
+    } else {
+      console.log('You are a old.'); 
+      // msg = 'You are old.'; 
+    }
+    // endWirte(msg, isFirstCheckOld, isEndLine)
   };
   this.yearPasses = function () {
     // Increment the age of the person in here
+    age += 1;
+    // countYearPass += 1;
+    // if (countYearPass === 3) {
+      // isFirstCheckOld = false;
+    // }
   };
 }
+
+// function endWirte(msg, isFirstCheckOld, isEndLine) {
+//   if (isFirstCheckOld || !isEndLine) {
+//     console.log(msg)
+//   } else if (!isFirstCheckOld && isEndLine) {
+//     process.stdout.write(`${msg}\r`)
+//   }
+// }
 
 function main() {
 
