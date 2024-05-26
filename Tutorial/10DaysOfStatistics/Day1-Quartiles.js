21f39df019f22dd53601a5921ecbc8d0667a41e7
kslee <ks@colorworld.kr>
2024-05-26 11:12:33 +0900

    modified:   Day1-Quartiles.js

--- Tutorial/10DaysOfStatistics/Day1-Quartiles.js
+++ Tutorial/10DaysOfStatistics/Day1-Quartiles.js
@@ -31,7 +31,36 @@ function readLine() {
 
 function quartiles(arr) {
   // Write your code here
+  arr.sort((x, y) => x - y);
+  // console.log(arr);
+  const i = arr.length;
+  const isOdd = (i % 2) === 1;
+  const mid = (i / 2) | 0; 
+  const mid2 = isOdd ? mid : mid - 1; 
+  const medianX = (arr[mid] + arr[mid2]) / 2;
+  // console.log((arr[mid] + arr[mid2]) / 2);
   
+  let lowerHalf, upperHalf;
+  lowerHalf = arr.slice(0, mid);
+  // console.log(lowerHalf);
+  upperHalf = arr.slice(mid2+1, i);
+  // console.log(upperHalf);
+  const medianL = median(lowerHalf);
+  // console.log(medianL);
+  const medianU = median(upperHalf);
+  // console.log(medianU);
+  return [medianL,medianX,medianU];
+}
+
+function median(arr) {
+  // arr.sort((x, y) => x - y);
+  const i = arr.length;
+  const isOdd = (i % 2) === 1;
+  const mid = (i / 2) | 0;
+  const mid2 = isOdd ? mid : mid - 1;
+  const median = (arr[mid] + arr[mid2]) / 2;
+  // console.log((arr[mid] + arr[mid2]) / 2);
+  return median;
 }
 
 function main() {
@@ -47,3 +76,9 @@ function main() {
 
   ws.end();
 }
+
+/* 
+note : 
+로컬에서 코드 실행 방법
+> OUTPUT_PATH=output.txt node Day1-Quartiles.js < Day1-Quartiles-input.txt
+ */
