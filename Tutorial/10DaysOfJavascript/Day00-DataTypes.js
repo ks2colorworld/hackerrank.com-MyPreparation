275b1f34bf06d08276f044ec9daf1c2cabc3a5f1
kslee <ks@colorworld.kr>
2024-05-26 18:30:13 +0900

    modified:   Day00-DataTypes.js

--- Tutorial/10DaysOfJavascript/Day00-DataTypes.js
+++ Tutorial/10DaysOfJavascript/Day00-DataTypes.js
@@ -46,11 +46,13 @@ function performOperation(secondInteger, secondDecimal, secondString) {
 
   // Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number        type) on a new line.
   // online : https://www.hackerrank.com/challenges/js10-data-types/problem?isFullScreen=true
+  console.log(firstInteger + parseInt(secondInteger))
 
   // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
-
+  console.log(firstDecimal + parseFloat(secondDecimal))
 
   // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
+  console.log(firstString + secondString);
 
 }
 
