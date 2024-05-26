944e8a772d2094c9098206c63424cc7a1998458e
kslee <ks@colorworld.kr>
2024-05-27 02:05:36 +0900

    modified:   Day1-StandardDeviation.js

--- Tutorial/10DaysOfStatistics/Day1-StandardDeviation.js
+++ Tutorial/10DaysOfStatistics/Day1-StandardDeviation.js
@@ -29,7 +29,36 @@ function readLine() {
 function stdDev(arr) {
   // Print your answers to 1 decimal place within this function
   // online: https://www.hackerrank.com/challenges/s10-standard-deviation/problem?isFullScreen=true
+  // 1. Mean of Data
+  let mean = 0, sumArr = 0;
+  const n = arr.length;
+  for (const v of arr) {
+    // console.log(v);
+    sumArr += v;
+  }
+  // sumArr = arr.reduce((acc,value)=>acc+value,0)
+  mean = sumArr / n;
+  console.log(mean);
 
+  // 2. Mean of Squared Differences(Data-1.)
+  let meanOfSquaredDiff = 0, sumSquaredDiff = 0;
+  for (const v  of arr) {
+    const diff = v - mean;
+    const squaredDiff = diff * diff;
+    sumSquaredDiff += squaredDiff;
+  }
+  // sumSquaredDiff = arr.reduce((acc, v) => {
+  //   const diff = v - mean;
+  //   const squaredDiff = diff * diff;
+  //   return acc + squaredDiff;
+  // }, 0);
+  // sumSquaredDiff = arr.reduce((acc, value) => acc + ((value - mean) * (value - mean)), 0);
+  meanOfSquaredDiff = sumSquaredDiff / n;
+  console.log(sumSquaredDiff);
+  console.log(meanOfSquaredDiff);
+  
+  // 3. square root of 2.
+  console.log(Math.sqrt(meanOfSquaredDiff).toFixed(1));
 }
 
 function main() {
@@ -39,3 +68,8 @@ function main() {
 
   stdDev(vals);
 }
+
+/* 
+.reduce()
+Math.sqrt()
+ */
