99776ce76be3ee5d2f0627bdc3dde8d725ac8c69
kslee <ks@colorworld.kr>
2024-05-27 18:37:19 +0900

    new file:   Day2-BasicProbability.js

---
+++ Tutorial/10DaysOfStatistics/Day2-BasicProbability.js
@@ -0,0 +1,10 @@
+// online : https://www.hackerrank.com/challenges/s10-mcq-1/problem
+
+/* 
+In a single toss of 2 fair (evenly-weighted) six-sided dice, find the probability that their sum will be at most 9.
+
+- 2/3
+- 5/6
+- 1/4
+- 1/6
+*/
