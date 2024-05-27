c5f5bc01bb4485e5eff22d92e8e8483c1c76defc
kslee <ks@colorworld.kr>
2024-05-27 16:00:37 +0900

    modified:   Day05-Loops.js

--- Tutorial/30DaysOfCode/Day05-Loops.js
+++ Tutorial/30DaysOfCode/Day05-Loops.js
@@ -25,5 +25,19 @@ function readLine() {
 function main() {
   const n = parseInt(readLine().trim(), 10);
   // online : https://www.hackerrank.com/challenges/30-loops/problem?isFullScreen=true
-  
+  // console.log(n);
+  for (let i = 1; i < 11; i++) {
+    console.log(`${n} x ${i} = ${n*i}`);
+  }
+  // const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // const arr = Array(10).fill().map((_, idx) => { return idx + 1 }) 
+  // console.log(arr);
+  // arr.map((i) => {
+  //   console.log(`${n} x ${i} = ${n * i}`);
+  // })
+  // console.log(arr2);
 }
+
+/* 
+> node Day05-Loops.js < Day05-Loops-input.txt
+ */
