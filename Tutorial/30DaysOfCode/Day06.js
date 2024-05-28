9f3d93afe2f0992989e3b7fa8abbce925f0282c8
kslee <ks@colorworld.kr>
2024-05-28 23:15:32 +0900

    modified:   Day06.js

--- Tutorial/30DaysOfCode/Day06.js
+++ Tutorial/30DaysOfCode/Day06.js
@@ -1,7 +1,27 @@
 function processData(input) {
   //Enter your code here
   // online : https://www.hackerrank.com/challenges/30-review-loop/problem?isFullScreen=true
-  
+  // console.log(input);
+  const arr = input.split('\n');
+  // console.log(arr);
+  const count = parseInt(arr[0]);
+  // console.log(count);
+  for (let i = 1; i < count+1; i++) {
+    const element = arr[i];
+    // console.log(element);
+    let evenText = '';
+    let oddText = '';
+    for (const j in element) {
+      // console.log(j, element[j]);
+      if (j%2===0) {
+        evenText = evenText + element[j];
+      } else {
+        oddText = oddText + element[j];
+      }
+    }
+    console.log(evenText + ' ' + oddText);
+    // console.log(`${evenText} ${oddText}`);
+  }
 }
 
 process.stdin.resume();
