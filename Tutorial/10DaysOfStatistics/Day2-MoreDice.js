3cdcbc20b3fec924d034453df5336f113175a77f
kslee <ks@colorworld.kr>
2024-05-28 23:52:08 +0900

    modified:   Day2-MoreDice.js

--- Tutorial/10DaysOfStatistics/Day2-MoreDice.js
+++ Tutorial/10DaysOfStatistics/Day2-MoreDice.js
@@ -10,3 +10,31 @@ find the probability that the values rolled by each die will be different and th
 - 5/6
 */
 
+// const dice1 = [1, 2, 3, 4, 5, 6];
+const dice1 = Array(6).fill().map((_, idx) => { return idx + 1 });
+const dice2 = Array.from(dice1);
+
+console.log(dice1, dice2);
+
+let allCase = 0;
+let thisCase = 0;
+for (const num1 of dice1) {
+  for (const num2 of dice2) {
+    allCase += 1;
+    if (num1+num2===6 && (num1 != num2)) {
+      thisCase += 1;
+    }
+  }
+}
+console.log(thisCase, allCase);
+const gcdValue = gcd(thisCase, allCase);
+const u = thisCase / gcdValue;
+const l = allCase / gcdValue;
+console.log(u + '/' + l);
+
+function gcd(x,y) {
+  if (y === 0) {
+    return x;
+  }
+  return gcd(y, x % y);
+}
