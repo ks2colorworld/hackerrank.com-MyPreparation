b02f6e0822ee47ded81703ebb89eafc0d5311b29
kslee <ks@colorworld.kr>
2024-05-28 23:22:00 +0900

    new file:   Day2-MoreDice.js

---
+++ Tutorial/10DaysOfStatistics/Day2-MoreDice.js
@@ -0,0 +1,12 @@
+// online : https://www.hackerrank.com/challenges/s10-mcq-2/problem?isFullScreen=true
+
+/* 
+In a single toss of 2 fair (evenly-weighted) six-sided dice, 
+find the probability that the values rolled by each die will be different and the two dice have a sum of 6.
+
+- 1/9
+- 1/6
+- 2/3
+- 5/6
+*/
+
