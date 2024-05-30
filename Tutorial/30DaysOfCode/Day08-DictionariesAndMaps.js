653fdb6298a79afcdbd94963dcf528945d5fd1c4
kslee <ks@colorworld.kr>
2024-05-30 18:20:49 +0900

    modified:   Day08-DictionariesAndMaps.js

--- Tutorial/30DaysOfCode/Day08-DictionariesAndMaps.js
+++ Tutorial/30DaysOfCode/Day08-DictionariesAndMaps.js
@@ -1,7 +1,27 @@
 function processData(input) {
   //Enter your code here
   // online : https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem?isFullScreen=true
-  
+  // console.log(input);
+  const inputDataArr=input.split('\n')
+  // console.log(inputDataArr);
+  const n = parseInt(inputDataArr[0]);
+  const endLine = inputDataArr.length;
+  let d = {};
+  for (let i = 1; i <= n; i++){
+    // console.log(inputDataArr[i]);
+    const e = inputDataArr[i].split(' ');
+    d[e[0]] = e[1];
+  }
+  for (let i = n + 1; i < endLine; i++) {
+    const key = inputDataArr[i]
+    if (Object.hasOwnProperty.call(d, key)) {
+      const value = d[key];
+      console.log(`${key}=${value}`);
+    } else {
+      console.log('Not found');
+    }
+  }
+  // console.log(d);
 } 
 
 process.stdin.resume();
