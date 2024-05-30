8b6a56e3cf73305ec226638a142efe3e941a8441
kslee <ks@colorworld.kr>
2024-05-30 17:26:42 +0900

    modified:   Day3-ConditonalProbability.js

--- Tutorial/10DaysOfStatistics/Day3-ConditonalProbability.js
+++ Tutorial/10DaysOfStatistics/Day3-ConditonalProbability.js
@@ -8,3 +8,11 @@ Suppose a family has 2 children, one of which is a boy. What is the probability 
 - 2 / 3
 - 1 / 9
  */
+
+
+/* 
+Simply put, when you have a previous condition you end up altering the samples.
+At first, if a couple has 2 children, you samples are (B,G)(B,B)(G,B)(G,G). 
+But now you know one of them is a boy, which changes your samples to (B,G)(B,B)(G,B). 
+Thus the probability is now 1/3.
+ */
