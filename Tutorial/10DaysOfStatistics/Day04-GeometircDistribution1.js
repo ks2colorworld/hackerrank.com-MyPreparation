2e49d85f7f6acbe715a6009a097d0b81c3f3f361
kslee <ks@colorworld.kr>
2024-05-31 10:12:19 +0900

    new file:   Day04-BinomialDistribution1.js 	new file:   Day04-BinomialDistribution2.js 	new file:   Day04-GeometircDistribution1.js 	new file:   Day04-GeometircDistribution2.js

---
+++ Tutorial/10DaysOfStatistics/Day04-GeometircDistribution1.js
@@ -0,0 +1,16 @@
+function processData(input) {
+  //Enter your code here
+  // online : https://www.hackerrank.com/challenges/s10-geometric-distribution-1/problem?isFullScreen=true
+
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+  _input += input;
+});
+
+process.stdin.on("end", function () {
+  processData(_input);
+});
