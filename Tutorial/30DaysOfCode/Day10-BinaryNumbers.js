50a42f6b138cf9c8b215916d78bf60ae0f3e075a
kslee <ks@colorworld.kr>
2024-06-03 10:21:47 +0900

    modified:   Tutorial/30DaysOfCode/Day10-BinaryNumbers.js

--- Tutorial/30DaysOfCode/Day10-BinaryNumbers.js
+++ Tutorial/30DaysOfCode/Day10-BinaryNumbers.js
@@ -23,6 +23,33 @@ function readLine() {
 
 
 function main() {
-  const n = parseInt(readLine().trim(), 10);
+  let n = parseInt(readLine().trim(), 10);
   // online : https://www.hackerrank.com/challenges/30-binary-numbers/problem?isFullScreen=true
+  // console.log(n);
+  let s = [];
+  while (n>0) {
+    const remainder = n % 2;
+    n = (n / 2) | 0;
+    s.push(remainder);
+  }
+  // console.log(s.reverse());
+  let max1Count = 0;
+  let count1 = 0;
+  for (const v of s) {
+    // console.log(v);
+    if (v === 1) {
+      count1 += 1;
+    } else {
+      count1 = 0;
+    }
+    if (max1Count < count1) {
+      max1Count = count1;
+    }
+
+  }
+  console.log(max1Count);
 }
+
+/* 
+❯ echo "130" | node Day10-BinaryNumbers.js
+ */
