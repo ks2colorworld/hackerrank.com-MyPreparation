5f7e4d708d36ab12eeaf8a4f391664fb884d98ff
kslee <ks@colorworld.kr>
2024-06-03 23:13:25 +0900

    modified:   Tutorial/10DaysOfStatistics/Day04-GeometircDistribution1.js

--- Tutorial/10DaysOfStatistics/Day04-GeometircDistribution1.js
+++ Tutorial/10DaysOfStatistics/Day04-GeometircDistribution1.js
@@ -1,7 +1,14 @@
 function processData(input) {
   //Enter your code here
   // online : https://www.hackerrank.com/challenges/s10-geometric-distribution-1/problem?isFullScreen=true
-  console.log(input);
+  const str = input.split('\n');
+  // console.log(str);
+  const prob = str[0].split(' ').map(v=>parseInt(v));
+  // console.log(prob);
+  const order = parseInt(str[1]);
+  // console.log(order);
+  const geoDist = Math.pow(prob[0] / prob[1], 1) * Math.pow(1 - prob[0] / prob[1], order - 1);
+  console.log(geoDist.toFixed(3));
 }
 
 process.stdin.resume();
@@ -19,3 +26,6 @@ process.stdin.on("end", function () {
 ❯ echo "1 3\n5" | node Day04-GeometircDistribution1.js 
  */
 
+/* 
+2/3 x 2/3 x 2/3 x 2/3 x 1/3
+ */
