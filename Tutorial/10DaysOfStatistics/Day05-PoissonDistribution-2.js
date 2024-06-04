b3d3392c14bf7aae36b8dba1106ee6ad28bea52b
kslee <ks@colorworld.kr>
2024-06-04 21:06:06 +0900

    modified:   Tutorial/10DaysOfStatistics/Day05-PoissonDistribution-2.js 	new file:   Tutorial/10DaysOfStatistics/Day05-PoissonDistribution-2.md

--- Tutorial/10DaysOfStatistics/Day05-PoissonDistribution-2.js
+++ Tutorial/10DaysOfStatistics/Day05-PoissonDistribution-2.js
@@ -1,7 +1,21 @@
 function processData(input) {
   //Enter your code here
   // online : https://www.hackerrank.com/challenges/s10-poisson-distribution-2/problem?isFullScreen=true
-  
+  // console.log(input);
+  const i = input.trim().split(' ').map(v => { return parseFloat(v.trim()) });
+  console.log(i[0], i[1]);
+  // X = 0.88 , C_A = 160+40X^2
+  const lambdaA = i[0];
+  const E_X2 = lambdaA + Math.pow(lambdaA, 2);
+  const CostA = 160 + 40 * E_X2;
+  console.log(CostA.toFixed(3));
+
+  // Y = 1.55 , C_B = 128 + 40Y^2
+  const lambdaB = i[1];
+  const E_Y2 = lambdaB + Math.pow(lambdaB, 2);
+  const CostB = 128 + 40 * E_Y2;
+  console.log(CostB.toFixed(3));
+
 }
 
 process.stdin.resume();
@@ -15,3 +29,6 @@ process.stdin.on("end", function () {
   processData(_input);
 });
 
+/* 
+> echo "0.88 1.55" | node Day05-PoissonDistribution-2.js
+ */
---
+++ Tutorial/10DaysOfStatistics/Day05-PoissonDistribution-2.md
@@ -0,0 +1,6 @@
+- The number of repairs, `X`, that machine `A` needs is a Poisson random variablewith mean `0.88`. The daily cost of operating `A` is $$C_A=160+40X^2$$
+- The number of repairs, `Y`, that machine `B` needs is a Poisson random variable
+with mean `1.55`. The daily cost of operating `B` is $$C_B = 128 + 40Y^2$$
+
+Assume that the repairs take a negligible amount of time and the machines are maintained nightly to ensure that they operate like new at the start of each day.
+Find and print the expected daily cost for each machine `A`, `B`.
