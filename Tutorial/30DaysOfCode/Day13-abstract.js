00a8b1bb0d4431a4c5e07aa5906a9c4f68511a57
kslee <ks@colorworld.kr>
2024-06-05 17:44:49 +0900

    modified:   Tutorial/30DaysOfCode/Day13-abstract.js 	new file:   Tutorial/30DaysOfCode/Day13-abstract-input.txt

--- Tutorial/30DaysOfCode/Day13-abstract.js
+++ Tutorial/30DaysOfCode/Day13-abstract.js
@@ -28,23 +28,34 @@ class Book {
 }
 
 // Declare your class here.
-/**   
-*   Class Constructor
-*   
-*   @param title The book's title.
-*   @param author The book's author.
-*   @param price The book's price.
-**/
-// Write your constructor here
-
-/**   
-*   Method Name: display
-*   
-*   Print the title, author, and price in the specified format.
-**/
-// Write your method here
-// online : https://www.hackerrank.com/challenges/30-abstract-classes/problem?isFullScreen=true
-
+class MyBook extends Book{
+  /**   
+  *   Class Constructor
+  *   
+  *   @param title The book's title.
+  *   @param author The book's author.
+  *   @param price The book's price.
+  **/
+  // Write your constructor here
+  constructor(title, author, price) {
+    // this.title = title;
+    // this.author = author;
+    super(title, author);
+    this.price = price;
+  }
+  /**   
+  *   Method Name: display
+  *   
+  *   Print the title, author, and price in the specified format.
+  **/
+  // Write your method here
+  // online : https://www.hackerrank.com/challenges/30-abstract-classes/problem?isFullScreen=true
+  display() {
+    console.log(`Title: ${this.title}
+Author: ${this.author}
+Price: ${this.price}`);
+  }
+}
 // End class
 
 function main() {
