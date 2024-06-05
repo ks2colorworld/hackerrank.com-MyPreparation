5c6ab7954bde53137d05dde9a15abcc2d1b4c095
kslee <ks@colorworld.kr>
2024-06-06 00:52:46 +0900

    modified:   Tutorial/10DaysOfJavascript/Day05-TemplateLiterals.js 	new file:   Tutorial/10DaysOfJavascript/Day05-TemplateLiterals.md

--- Tutorial/10DaysOfJavascript/Day05-TemplateLiterals.js
+++ Tutorial/10DaysOfJavascript/Day05-TemplateLiterals.js
@@ -33,18 +33,35 @@ function readLine() {
  */
 //  online : https://www.hackerrank.com/challenges/js10-template-literals/problem?isFullScreen=true
 function sides(literals, ...expressions) {
+  console.log(literals);
+  console.log(expressions);
+  const A = expressions[0];
+  const P = expressions[1];
+  console.log('A:',A,'P:',P);
 
+  const sqrtPart = Math.sqrt(P * P - 16 * A);
+
+  const s1 = (P + sqrtPart) / 4;
+  const s2 = (P - sqrtPart) / 4;
+  console.log(s1, s2);
+  return [s1, s2].sort();
 }
 
 
 function main() {
   let s1 = +(readLine());
   let s2 = +(readLine());
-
+  console.log(s1,s2);
+  
   [s1, s2] = [s1, s2].sort();
+  console.log(s1,s2);
 
   const [x, y] = sides`The area is: ${s1 * s2}.\nThe perimeter is: ${2 * (s1 + s2)}.`;
 
   console.log((s1 === x) ? s1 : -1);
   console.log((s2 === y) ? s2 : -1);
 }
+
+/* 
+> echo "10\n14" | node Day05-TemplateLiterals.js
+ */
