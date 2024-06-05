97942706d951916db4648bd3ceaf2c9e2bf657d8
kslee <ks@colorworld.kr>
2024-06-05 10:35:13 +0900

    new file:   Tutorial/30DaysOfCode/Day11-2D-Arrays.js 	new file:   Tutorial/30DaysOfCode/Day12-inheritance.js 	new file:   Tutorial/30DaysOfCode/Day13-abstract.js 	new file:   Tutorial/30DaysOfCode/Day14-scope.js

---
+++ Tutorial/30DaysOfCode/Day11-2D-Arrays.js
@@ -0,0 +1,34 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+  inputString = inputString.split('\n');
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+
+
+function main() {
+
+  let arr = Array(6);
+
+  for (let i = 0; i < 6; i++) {
+    arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+  }
+  // online : https://www.hackerrank.com/challenges/30-2d-arrays/problem?isFullScreen=true
+  
+}
