18a03e0b2db621c87412de0a366a2f79b34b2f94
kslee <ks@colorworld.kr>
2024-06-06 18:54:51 +0900

    modified:   Tutorial/10DaysOfJavascript/Day06-BitwiseOperators.js 	new file:   Tutorial/10DaysOfJavascript/Day06-BitwiseOperators-input.txt

--- Tutorial/10DaysOfJavascript/Day06-BitwiseOperators.js
+++ Tutorial/10DaysOfJavascript/Day06-BitwiseOperators.js
@@ -23,6 +23,32 @@ function readLine() {
 }
 
 // online : https://www.hackerrank.com/challenges/js10-bitwise/problem?isFullScreen=true
+function getMaxLessThanK(n,k) {
+  // console.log(n, k);
+  let arr = Array(n).fill().map((_, i) => { return i + 1 });
+  // for (let i = 0; i < n; i++){
+  //   a[i] = i + 1;
+  // }
+  // console.log(arr);
+  let maxAandB = 0;
+  for (let i = 0; i < arr.length; i++) {
+    const a = arr[i];
+    for (let j = i+1; j < arr.length; j++) {
+      const b = arr[j];
+      // console.log('a:', a, 'b:', b, 'a&b:', a & b);
+      const aANDb = a & b;
+      // const bitA = '0'.repeat(4 - a.toString(2).length) + a.toString(2);
+      // const bitB = '0'.repeat(4 - b.toString(2).length) + b.toString(2);
+      // const bitAandB = '0'.repeat(4 - aANDb.toString(2).length) + aANDb.toString(2);
+      // console.log(bitA, bitB, bitAandB);
+      if (maxAandB < aANDb && aANDb < k) {
+        maxAandB = aANDb;
+        // console.log(maxAandB);
+      }
+    }
+  }
+  return maxAandB;
+}
 
 function main() {
   const q = +(readLine());
