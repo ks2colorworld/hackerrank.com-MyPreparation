1a4a6e4c6ee89e8a64feecd6203f7b58e7565390
kslee <ks@colorworld.kr>
2024-06-07 18:54:06 +0900

    modified:   Tutorial/30DaysOfCode/Day16-Exceptions.js 	new file:   Tutorial/30DaysOfCode/Day16-Exceptions-input.txt

--- Tutorial/30DaysOfCode/Day16-Exceptions.js
+++ Tutorial/30DaysOfCode/Day16-Exceptions.js
@@ -25,5 +25,14 @@ function readLine() {
 function main() {
   const S = readLine();
   // online : https://www.hackerrank.com/challenges/30-exceptions-string-to-integer/problem?isFullScreen=true
+  const n = parseInt(S);
+  console.log(!!n? n : 'Bad String');
   
+  // let s = S;
+  // for (let i = 0; i < inputString.length; i++){
+  //   console.log(s);
+  //   const n = parseInt(s);
+  //   console.log(!!n? n : 'Bad String');
+  //   s = readLine();
+  // }
 }
