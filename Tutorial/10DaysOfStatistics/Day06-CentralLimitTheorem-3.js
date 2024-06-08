5f67bc6554e1ac7dab3365b48ec578b1eed11f94
kslee <ks@colorworld.kr>
2024-06-08 10:19:37 +0900

    modified:   Tutorial/10DaysOfStatistics/Day06-CentralLimitTheorem-3.js 	new file:   Tutorial/10DaysOfStatistics/Day06-CentralLimitTheorem-3-input.txt

--- Tutorial/10DaysOfStatistics/Day06-CentralLimitTheorem-3.js
+++ Tutorial/10DaysOfStatistics/Day06-CentralLimitTheorem-3.js
@@ -1,6 +1,19 @@
 function processData(input) {
   //Enter your code here
-  
+
+  const arr = input.trim().split('\n').map(v => parseFloat(v));
+  //   console.log(arr);
+
+  const n = arr[0]; // 100;
+  const mean = arr[1]; // 500;
+  const std = arr[2]; // 80;
+  const zScore = arr[4]; // 1.96;
+
+  const A = mean - zScore * (std / Math.sqrt(n))
+  const B = mean + zScore * (std / Math.sqrt(n))
+
+  console.log(A.toFixed(2));
+  console.log(B.toFixed(2));
 }
 
 process.stdin.resume();
