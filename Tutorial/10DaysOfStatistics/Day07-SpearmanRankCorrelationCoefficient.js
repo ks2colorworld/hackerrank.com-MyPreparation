370beeb712b83eb0cd2675a458f171d67a1330e2
kslee <ks@colorworld.kr>
2024-06-10 08:32:11 +0900

    modified:   Tutorial/10DaysOfStatistics/Day07-SpearmanRankCorrelationCoefficient.js

--- Tutorial/10DaysOfStatistics/Day07-SpearmanRankCorrelationCoefficient.js
+++ Tutorial/10DaysOfStatistics/Day07-SpearmanRankCorrelationCoefficient.js
@@ -1,6 +1,15 @@
 // online : https://www.hackerrank.com/challenges/s10-spearman-rank-correlation-coefficient/problem?isFullScreen=true
 function processData(input) {
   //Enter your code here
+  console.log(input);
+  const _i = input.trim().split('\n');
+
+  const X = _i[1].trim().split(' ').map(v => parseFloat(v));
+  const Y = _i[2].trim().split(' ').map(v => parseFloat(v));
+  console.log(X, Y);
+
+  const r = spearmanCorrelation(X, Y);
+  console.log(r.toFixed(3));
 }
 
 process.stdin.resume();
@@ -13,3 +22,26 @@ process.stdin.on("data", function (input) {
 process.stdin.on("end", function () {
   processData(_input);
 });
+
+function spearmanCorrelation(X, Y) {
+  const rank = (arr) => {
+    const sorted = [...arr].sort((a, b) => a - b);
+    return arr.map(v => sorted.indexOf(v) + 1);
+  };
+
+  const rank_X = rank(X);
+  const rank_Y = rank(Y);
+
+  const d = rank_X.map((rankX, i) => rankX - rank_Y[i]);
+  const d_squared_sum = d.map(di => di ** 2).reduce((a, b) => a + b, 0);
+
+  const n = X.length;
+  const rho = 1 - (6 * d_squared_sum) / (n * (n ** 2 - 1));
+
+  return rho;
+}
+
+/* 
+node Day07-SpearmanRankCorrelationCoefficient.js < Day07-SpearmanRankCorrelationCoefficient-input.txt
+
+ */
