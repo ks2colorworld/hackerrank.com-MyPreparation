a8d452083742db2e1f3b0ac6d5625c8b5d48948b
kslee <ks@colorworld.kr>
2024-06-10 08:16:23 +0900

    new file:   Tutorial/10DaysOfStatistics/Day07-SpearmanRankCorrelationCoefficient.js

---
+++ Tutorial/10DaysOfStatistics/Day07-SpearmanRankCorrelationCoefficient.js
@@ -0,0 +1,15 @@
+// online : https://www.hackerrank.com/challenges/s10-spearman-rank-correlation-coefficient/problem?isFullScreen=true
+function processData(input) {
+  //Enter your code here
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+  _input += input;
+});
+
+process.stdin.on("end", function () {
+  processData(_input);
+});
