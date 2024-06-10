6f5f5e80c3fe3781144927af1a82acf6d297acb7
kslee <ks@colorworld.kr>
2024-06-10 21:42:11 +0900

    new file:   Tutorial/10DaysOfStatistics/Day08-LeastSquareRegressionLine.js

---
+++ Tutorial/10DaysOfStatistics/Day08-LeastSquareRegressionLine.js
@@ -0,0 +1,19 @@
+function processData(input) {
+  //Enter your code here
+  // online : https://www.hackerrank.com/challenges/s10-least-square-regression-line/problem?isFullScreen=true
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+  _input += input;
+});
+
+process.stdin.on("end", function () {
+  processData(_input);
+});
+
+/* 
+❯ node Day08-LeastSquareRegressionLine.js < Day08-LeastSquareRegressionLine-input.txt
+ */
